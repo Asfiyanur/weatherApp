26f340337dc8f179e104014b5dd1a0c0caaebe7c
Asfiyanur <nurasfiya.13@gmail.com>
2022-09-30 11:00:15 +0300

    function added

--- HTML-CSS/weather.js
+++ HTML-CSS/weather.js
@@ -2,3 +2,19 @@ const form = document.querySelector(".top-banner form");
 const input = document.querySelector("container input");
 const msg = document.querySelector("span.msg");
 const list = document.querySelector(".ajax-section ul.cities");
+
+localStorage.setItem(
+  "tokenKEY",
+  "C1ZYgfcMAb+dmIZOlfiU9p+RwnMqDHPrOXOZW4choVbGG88NckpqdAn8vVPW2BUN"
+);
+// localStorage.setItem(
+//   "tokenKEYEncrypted",
+//   EncryptStringAES("572b492afc3865f13b927ef035e82171")
+// );
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  getWeatherDataFromApi();
+});
+
+const getWeatherDataFromApi = () => {};
